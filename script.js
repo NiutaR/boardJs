b3d54351789c9188baec9b5da7da04070be436e1
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-02-07 13:03:58 +0200

    board project

---
+++ script.js
@@ -0,0 +1,30 @@
+const board = document.getElementById('board');
+const colors = ['#a5a4fe', '#f00580', '#cbba37', '#cbba37', '#039c64', '#d5cabc', '#d5cabc'];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+    board.append(square);
+    square.addEventListener('mouseleave', () => removeColor(square));
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
